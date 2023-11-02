27d575c247e980e7fb3f0d326a1ef16ccd5cf62b
Chameleon2die4 <chameleon2die4@gmail.com>
2023-11-02 08:49:58 +0100

    Fix Route

--- src/Route/Route.php
+++ src/Route/Route.php
@@ -9,62 +9,72 @@ class Route extends BaseRoute
 
     /**
      * @param string $url
-     * @param string|array $callback
+     * @param $callback
+     * @param array $args
      * @return void
      */
-    public static function get(string $url, $callback, $args = []) {
+    public static function get(string $url, $callback, $args = [])
+    {
         self::callMethod('get', $url, $callback, $args);
     }
 
     /**
      * @param string $url
-     * @param string|array $callback
+     * @param $callback
+     * @param $args
      * @return void
      */
-    public static function post(string $url, $callback, $args = []) {
+    public static function post(string $url, $callback, $args = [])
+    {
         self::callMethod('post', $url, $callback, $args);
     }
 
     /**
      * @param string $url
-     * @param string|array $callback
+     * @param $callback
+     * @param array $args
      * @return void
      */
-    public static function put(string $url, $callback, $args = []) {
+    public static function put(string $url, $callback, $args = [])
+    {
         self::callMethod('put', $url, $callback, $args);
     }
 
     /**
      * @param string $url
-     * @param string|array $callback
+     * @param $callback
+     * @param array $args
      * @return void
-     * @noinspection PhpUnused
      */
-    public static function patch(string $url, $callback, $args = []) {
+    public static function patch(string $url, $callback, $args = [])
+    {
         self::callMethod('patch', $url, $callback, $args);
     }
 
     /**
      * @param string $url
-     * @param string|array $callback
+     * @param $callback
+     * @param array $args
      * @return void
      */
-    public static function delete(string $url, $callback, $args) {
+    public static function delete(string $url, $callback, $args = [])
+    {
         self::callMethod('delete', $url, $callback, $args);
     }
 
     /**
      * @param string $method
      * @param string $url
-     * @param mixed $callback
+     * @param $callback
+     * @param array $args
      * @return void
      */
-    public static function callMethod(string $method, string $url, $callback, $args = []) {
+    public static function callMethod(string $method, string $url, $callback, $args = [])
+    {
         if (is_array($callback)) {
             $callback = implode('@', $callback);
         }
 
         call_user_func_array([parent::class, $method], [$url, $callback, $args]);
     }
-
 }
